ecf07e9b21b83c2d5339d5f28f76352ecbfccaaa
vladimircherepanov <51172219+vladimircherepanov@users.noreply.github.com>
2021-02-28 14:03:41 +0300

    xxx

--- src/components/CurrencyPicker.js
+++ src/components/CurrencyPicker.js
@@ -9,10 +9,7 @@ export default () => {
 
   return (
     <div className="currency">
-      <select
-        className="form-control"
-        onChange={(event) => dispatch(getCurrency(event.target.value))}
-      >
+      <select onChange={(event) => dispatch(getCurrency(event.target.value))}>
         <option value="USD">{t("USD")}</option>
         <option value="EUR">{t("EUR")}</option>
         <option value="RUB">{t("RUB")}</option>
